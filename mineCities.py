2780fd35a6ca3d93ac04ecd14d4739f6e840f7c9
Wren Vogelschmidt <wvogelschmidt@colgate.edu>
2021-02-14 10:45:47 -0500

    using Wikipedia REST API

---
+++ mineCities.py
@@ -0,0 +1,101 @@
+import pageviewapi
+import wikipedia
+import requests
+import json
+import csv
+import time
+
+
+def parseCity(line):
+    output = "" 
+    for char in line:
+        if(char == ','):
+            return output[11:]
+        output += char
+
+def parsePopulation(line):
+    output = ""
+    for char in line:
+        if (char.isdigit()):
+            output += char
+    return output
+        
+def parseName(line):
+    output = ""
+    for char in line:
+        if(char == ']' or char == '|'):
+            if (char == ')'):
+                output += char
+            return output[3:]
+        output += char
+
+def createList():
+    file = open('CitiesInIowa.xml', 'r')
+    count = 0
+    notableArr = [[]]
+    cityArr = []
+    cityName = ""
+    addPeople = False
+
+    for line in file:
+        count = count + 1
+        if ("<title>" in line and ("Category:" not in line)):
+            if (len(cityArr) > 1):
+                notableArr.append(cityArr)
+            cityArr = []
+            addPeople = False
+            cityName = parseCity(line)
+            cityArr = [cityName]
+        if ("population_total" in line):
+            cityArr.append(parsePopulation(line))
+        if ("==" in line and "Notable" in line):
+            addPeople = True
+        if ("==" in line and not "Notable" in line):
+            addPeople = False
+        if((addPeople) and ("*[[" in line) and ("Category:" not in line) and ("File:" not in line) and ("NULLIFIED" not in line) and ("END OF NOTICE" not in line)):
+            cityArr.append(parseName(line))
+            
+    file.close()
+    return notableArr
+
+def getPageViews(lis):
+    article = wikipedia.page(lis).url
+    article = article[30:]
+    url = "https://en.wikipedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/" + article + "/monthly/20210101/20210201"
+    print(url)
+    heads = {'User-Agent': '(wvogelschmidt@colgate.edu)'}
+    if (requests.get(url, headers=heads) == 200):
+        resp = requests.get(url, headers=heads)
+        rd = resp.json()
+        return rd['items'][0]['views']
+    else:
+        return 0
+
+
+def outputList(notableArr):
+    file = open('output.csv', 'w')
+
+    for array in notableArr:
+        if(len(array) > 2):
+            count = 0
+            pageViews = 0
+            maxViews = 0
+            for lis in array:
+                if(count < 2):
+                    file.write(lis)
+                    file.write(',')
+                    count += 1
+                else:
+                    pgVw = getPageViews(lis)
+                    if (pgVw > maxViews):
+                        maxViews = pgVw
+                    pageViews += pgVw
+            file.write(str(len(array)-2) + ',')
+            file.write(str(pageViews) + ',')
+            file.write(str(maxViews))
+            file.write('\n')
+    file.close()
+
+#Create and Output List
+notableArr = createList()
+outputList(notableArr)
